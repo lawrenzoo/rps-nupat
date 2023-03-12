770cd72d5e6114dd33396dfe71ccf240737a83b4
lawrenzoo <chiagozielawrence@gmail.com>
2023-03-12 20:22:23 +0100

    lawrenxoo work coder

--- script.js
+++ script.js
@@ -1,156 +1,131 @@
+//
+
 const checkChoice = {
 	0: "rock",
 	1: "paper",
 	2: "scissors",
-};
-
-// computer and a player
-let playerChoice;
-let computerChoice;
-
-let playerScore = 0;
-let computerScore = 0;
-
-let flag = true;
-
-let mychecker;
-while (flag) {
+  };
+  
+  // computer and a player
+  let playerChoice;
+  let computerChoice;
+  
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  let flag = true;
+  
+  while (flag) {
 	const startGame = prompt(
-		"Do you want to play Rock Paper Scissors? | yes or exit to end the game"
+	  "Do you want to play Rock Paper Scissors? | yes or exit to end the game"
 	);
-
+  
 	if (startGame !== "exit" && startGame === "yes") {
-		mychecker = 1;
-		alert("computer turn");
-		computerChoice = Math.floor(Math.random() * 3);
-
-		alert("Player turn");
-		playerChoice = Number(prompt("0 = rock, 1 = paper or 2 = scissors"));
-
-		if (computerChoice === 0) {
-			switch (playerChoice) {
-				case 1:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -Player wins`
-					);
-					playerScore += 5;
-					break;
-				case 2:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -Computer wins`
-					);
-					computerScore += 5;
-					break;
-				case 0:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -It is a draw`
-					);
-					break;
-				default:
-					alert("please enter either 1, 0 or 2");
-			}
-		} else if (computerChoice === 1) {
-			switch (playerChoice) {
-				case 1:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -It is a draw`
-					);
-					break;
-				case 2:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -Player wins`
-					);
-					playerScore += 5;
-					break;
-				case 0:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -Computer wins`
-					);
-					computerScore += 5;
-					break;
-				default:
-					alert("please enter either 1, 0 or 2");
-			}
-		} else {
-			switch (playerChoice) {
-				case 1:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -Computer wins`
-					);
-					computerScore += 5;
-					break;
-				case 2:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -It is a draw`
-					);
-					break;
-				case 0:
-					alert(
-						`computer chose: "${checkChoice[computerChoice]}" -Player wins`
-					);
-					playerScore += 5;
-					break;
-				default:
-					alert("please enter either 1, 0 or 2");
-			}
+	  alert("computer turn");
+	  computerChoice = Math.floor(Math.random() * 3);
+  
+	  alert("Player turn");
+	  playerChoice = Number(prompt("0 = rock, 1 = paper or 2 = scissors"));
+  
+	  if (computerChoice === 0) {
+		switch (playerChoice) {
+		  case 1:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -Player wins`
+			);
+			playerScore += 5;
+			break;
+		  case 2:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -Computer wins`
+			);
+			computerScore += 5;
+			break;
+		  case 0:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -It is a draw`
+			);
+			break;
+		  default:
+			alert("please enter either 1, 0 or 2");
 		}
-
-		alert(
-			"Player score = " +
-				playerScore +
-				"pts" +
-				"\n" +
-				"Computer score = " +
-				computerScore +
-				"pts"
-		);
-
-		playerScore === computerScore
-			? alert("It's a draw")
-			: playerScore > computerScore
-			? alert("Player wins this round")
-			: alert("Computer wins this round");
+	  } else if (computerChoice === 1) {
+		switch (playerChoice) {
+		  case 1:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -It is a draw`
+			);
+			break;
+		  case 2:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -Player wins`
+			);
+			playerScore += 5;
+			break;
+		  case 0:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -Computer wins`
+			);
+			computerScore += 5;
+			break;
+		  default:
+			alert("please enter either 1, 0 or 2");
+		}
+	  } else {
+		switch (playerChoice) {
+		  case 1:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -Computer wins`
+			);
+			computerScore += 5;
+			break;
+		  case 2:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -It is a draw`
+			);
+			break;
+		  case 0:
+			alert(
+			  `computer chose: "${checkChoice[computerChoice]}" -Player wins`
+			);
+			playerScore += 5;
+			break;
+		  default:
+			alert("please enter either 1, 0 or 2");
+		}
+	  }
+  
+	  alert(
+		"Player score = " +
+		  playerScore +
+		  "pts" +
+		  "\n" +
+		  "Computer score = " +
+		  computerScore +
+		  "pts"
+	  );
+  
+	  playerScore === computerScore
+		? alert("It's a draw")
+		: playerScore > computerScore
+		? alert("Player wins this round")
+		: alert("Computer wins this round");
 	} else {
-		if (playerScore > computerScore) {
-			let highestScore = playerScore;
-			//  alert("Total Player Score " + highestScore + "pts \n" + "Total Computer Score " + computerScore + "\n"+ "Highest Score:  Player - Winner");
-			console.log(
-				"Total Player Score: " +
-					highestScore +
-					"pts \n" +
-					"Total Computer Score: " +
-					computerScore +
-					" pts" +
-					"\n" +
-					"Highest Score:  Player  Winner"
-			);
-		} else if (computerScore > playerScore) {
-			let highestScore = computerScore;
-			// alert("Total Computer score " + highestScore + "pts \n" + "Total Player Score"  + playerScore + "\n" + "Highest Score:  Computer - Winner");
-			console.log(
-				"Total Computer score: " +
-					highestScore +
-					"pts \n" +
-					"Total Player Score: " +
-					playerScore +
-					" pts" +
-					"\n" +
-					"Highest Score:  Computer  Winner"
-			);
-		} else {
-			if (mychecker == 1) {
-				highestScore = computerScore;
-				// alert("Draw by  computer and player" + highestScore + " pts \n" + "No - Winner");
-				console.log(
-					"Draw by  computer and player: " +
-						highestScore +
-						" pts \n" +
-						"No Winner"
-				);
-			} else {
-				console.log("game didnt start");
-			}
-		}
-		flag = false;
+		   if(playerScore > computerScore) {
+		let highestScore = playerScore;
+	  
+	 console.log("Total Player Score: " + highestScore + "pts \n" + "Total Computer Score " + computerScore + "\n"+ "Highest Score:  Player  Winner");
+	  }else if (computerScore > playerScore){
+		let highestScore = computerScore;
+	   
+	  console.log("Total Computer score: " + highestScore + "pts \n" + "Total Player Score"  + playerScore + "\n" + "Highest Score:  Computer  Winner");
+	  }
+	  else{
+		highestScore = computerScore;
+	 console.log("Draw by  computer and player: " + highestScore + " pts \n" + "No Winner");
+	  }
+	  flag = false;
 	}
-}
+	
+  }
+  
